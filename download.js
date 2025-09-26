4a5537e33cc157aa829ca3a4935276d05c385fb8
portalplayer <kratcy@fuckb.ing>
2025-09-26 01:38:16 -0400

    Download: Parse the README

--- download.js
+++ download.js
@@ -2,7 +2,7 @@
 // Created on: 9/26/25 @ 1:24 AM
 
 const {execSync} = require("child_process")
-const {rmSync, existsSync} = require("fs")
+const {rmSync, existsSync, readFileSync} = require("fs")
 
 if (existsSync("output")) {
     console.log("Removing old output")
@@ -13,3 +13,16 @@ if (existsSync("output")) {
 
 console.log("Downloading master")
 execSync("git clone http://10.0.44.20:3000/Passwords/Master output")
+console.log("Parsing optional password databases")
+
+const readmeOriginal = readFileSync("output/README.md").toString().trim()
+let urls = {}
+
+for (const project of readmeOriginal.split("\n")) {
+    let name = project.substring(3)
+    let url = project.substring(project.indexOf("(") + 1)
+
+    name = name.substring(0, name.indexOf("]"))
+    url = url.substring(0, url.length - 1)
+    urls[name] = url
+}
