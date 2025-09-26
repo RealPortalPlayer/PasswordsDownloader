b0bdf68092407b617a71f962b3579009fa247eee
portalplayer <kratcy@fuckb.ing>
2025-09-26 01:33:01 -0400

    Download: Delete old output if it exists

--- download.js
+++ download.js
@@ -2,6 +2,14 @@
 // Created on: 9/26/25 @ 1:24 AM
 
 const {execSync} = require("child_process")
+const {rmSync, existsSync} = require("fs")
+
+if (existsSync("output")) {
+    console.log("Removing old output")
+    rmSync("output", {
+        recursive: true
+    })
+}
 
 console.log("Downloading master")
 execSync("git clone http://10.0.44.20:3000/Passwords/Master output")
